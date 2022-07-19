bc8d407fa4abfa5ba5940ad7b511e0fea539bf8f
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-07-18 21:44:43 -0600

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -18,10 +18,10 @@ const MyApp =  ({ Component, pageProps }: AppProps) =>  {
 
   return (
     <div>
-      Navbar
+      <Navbar />
       <div className="flex gap-6 md:gap-20">
         <div className="h-[92vh] overflow-hidden xl:">
-          Sidebar
+          <Sidebar />
         </div>
         <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
           <Component {...pageProps} />
