569be50553c235ea1da6bcef038cc52a2d4fc8c8
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-07-19 19:52:52 -0600

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -6,6 +6,7 @@ import Link from 'next/link'
 import GoogleLogin from 'react-google-login'
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai'
 import { ImCancelCircle } from 'react-icons/im'
+import Discover from './Discover'
 
 
 const Sidebar = () => {
