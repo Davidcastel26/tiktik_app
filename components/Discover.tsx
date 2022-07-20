ab672a0f9669212d599a91beed41d1e7d3c8fe74
David Castellanos <49693134+Davidcastel26@users.noreply.github.com>
2022-07-19 19:56:55 -0600

    Update Discover.tsx

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+import { topics } from '../utils/constants'
 
 const Discover = () => {
   return (
-    <div>Discover</div>
+    <div>
+
+    </div>
   )
 }
 
